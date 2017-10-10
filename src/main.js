5bf39912ab4ca5162ae2c37414b0223e94ac3ec1
Alexandre De Pablo <alexandre.depablo@gmail.com>
2017-10-10 14:54:10 +0200

    Setting up vuetify

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Vuetify from 'vuetify'
 import router from './router'
+import('../node_modules/vuetify/dist/vuetify.min.css')
 
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
