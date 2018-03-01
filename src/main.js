5cd70bfe2c475a322f004066c8a96f3c80f77715
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-03-01 12:29:34 +0100

    Remove axios instance from Vue app

--- src/main.js
+++ src/main.js
@@ -4,16 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import VeeValidate from 'vee-validate'
 import router from './router'
-import axios from './axios-common'
 import store from './store'
 
 require('@/style/app.scss')
 
 Vue.use(VeeValidate)
 Vue.config.productionTip = false
-
-// $http is now available from anywhere in the app
-Vue.prototype.$http = axios
 
 /* eslint-disable no-new */
 new Vue({
