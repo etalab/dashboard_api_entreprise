6508d78c005b1c23a04fe4505b064cd89e2edb2a
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-03-08 16:37:33 +0100

    Change base url for admin APIs

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_BASE_URL: '"https://watchdoge.entreprise.api.gouv.fr"',
+  API_BASE_URL: '"https://sandbox.dashboard.entreprise.api.gouv.fr"',
   API_URL_PREFIX: '"/api/admin"'
 }
