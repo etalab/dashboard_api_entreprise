c037a6c2fd4977406cd7b21420f97e729e8b03ad
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-03-09 12:40:35 +0100

    Fix error server handling

--- src/store/api/admin/index.js
+++ src/store/api/admin/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import router from '@/router'
 
 const API_URL = `${process.env.API_BASE_URL}${process.env.API_URL_PREFIX}`
 
@@ -16,7 +17,7 @@ const actions = {
     return new Promise((resolve, reject) => {
       http.get(url, { params })
         .then(response => resolve(response.data))
-        .catch(error => ctx.dispatch('handleError', error))
+        .catch(error => reject(handleError(error)))
     })
   },
 
@@ -24,19 +25,20 @@ const actions = {
     return new Promise((resolve, reject) => {
       http.post(url, params)
         .then(response => resolve(response.data))
-        .catch(error => ctx.dispatch('handleError', error))
+        .catch(error => reject(handleError(error)))
     })
   },
 
   updateAuthorizationBearer (ctx) {
     http.defaults.headers['Authorization'] = 'Bearer ' + localStorage.token
-  },
-
-  handleError ({ dispatch }, error) {
-    // TODO handle error message with vuex
-    if (!error.response) dispatch('router/push', 'logout', { root: true })
-    else if (error.response.status === 401) dispatch('router/push', 'logout', { root: true })
   }
+}
+
+const handleError = (error) => {
+  // TODO handle error message with vuex
+  if (!error.response) router.push({ name: 'logout' })
+  else if (error.response.status === 401) router.push({ name: 'logout' })
+  else if (error.response.status === 422) return error.response.data.errors
 }
 
 export default { namespaced: true, actions }
