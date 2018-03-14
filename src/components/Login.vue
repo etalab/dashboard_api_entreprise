ddd085f019f0785869ba69ac025893a781358773
Thimy Kieu <thimy.nya@gmail.com>
2018-03-14 17:41:11 +0100

    fix content position

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  div
+  .content
     navigation-header
 
     .login
--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  div
+  .content
     navigation-header
     router-view
 </template>
