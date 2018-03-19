633c93446ac6bb5567c2bb8a7014091570ce4f01
Alexandre De Pablo <alexandre.depablo@gmail.com>
2018-03-19 18:06:24 +0100

    Update watchdoge URL prefix

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   API_BASE_URL: '"http://localhost:3000"',
-  DASHBOARD_URL_PREFIX: '"/dashboard"'
+  DASHBOARD_URL_PREFIX: '"/api/watchdoge/dashboard"'
 })
