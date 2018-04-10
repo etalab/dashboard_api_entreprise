c48f7bb4f1e80f37450d4bc928f3da0f184e01f9
Thimy Kieu <thimy.nya@gmail.com>
2018-04-10 17:59:42 +0100

    make tabs to separate profile

---
+++ src/components/resource/user/UserTokens.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  .profile__group
+    h2 Tokens de l'utilisateur
+    jwt-api-entreprise-index(:jwtList="tokens" v-if="tokens.length > 0")
+    p(v-else) Aucun token attribué
+
+    jwt-api-entreprise-new(v-if="isAdmin || allowedToCreateToken")
+</template>
+
+<script>
+import JWTAPIEntrepriseNew from '@/components/resource/jwt_api_entreprise/New'
+import JWTAPIEntrepriseIndex from '@/components/resource/jwt_api_entreprise/Index'
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'user-tokens',
+
+  computed: {
+    ...mapGetters({
+      tokens: 'user/tokens',
+      allowedToCreateToken: 'user/allowedToCreateToken',
+      isAdmin: 'auth/isAdmin'
+    })
+  },
+
+  components: {
+    'jwt-api-entreprise-new': JWTAPIEntrepriseNew,
+    'jwt-api-entreprise-index': JWTAPIEntrepriseIndex
+  }
+}
+</script>
