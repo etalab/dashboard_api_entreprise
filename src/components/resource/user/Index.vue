f237b60b898a55e499f0813940e875b8f02dd251
Alexis Leclerc <alexis.leclerc@data.gouv.fr>
2019-05-05 15:13:07 +0300

    Add v-data-table & seach

--- src/components/resource/user/Index.vue
+++ src/components/resource/user/Index.vue
@@ -1,23 +1,27 @@
 <template lang="pug">
   .main
-    h2 Liste des utilisateurs
-    button.button.small.title-button(@click="userForm") Ajouter un utilisateur
     .panel
-      table.table
-        thead
-          tr
-            th.text-left(v-for="header in headers") {{header.text}}
-        tbody
-          tr(v-for="user in userList")
+      .panel__header
+        h2 Liste des utilisateurs
+        button.button.small.title-button(@click="userForm") Ajouter un utilisateur
+
+      .form__group
+        input(v-model="search", placeholder="recherche par UUID, email, contexte")
+
+      .form__group
+        v-data-table.elevation-1(:headers="headers", :items="userList", :search="search")
+          template(v-slot:items="props")
             td
-              router-link(:to="{ name: 'admin-user-profile', params: { userId: user.id }}") {{ user.email }}
-            td {{ user.context }}
-            td {{ user.confirmed ? 'Oui' : 'Non' }}
+              router-link(:to="{ name: 'admin-user-profile', params: { userId: props.item.id }}") {{ props.item.email }}
+            td {{ props.item.context }}
+            td {{ props.item.confirmed ? 'Oui' : 'Non' }}
+            td.hidden {{ props.item.id }}
 
 </template>
 
 <script>
 import { createNamespacedHelpers } from 'vuex'
+import 'vuetify/dist/vuetify.min.css'
 const { mapGetters } = createNamespacedHelpers('user/index')
 
 export default {
@@ -26,10 +30,12 @@ export default {
   data () {
     return {
       title: 'Utilisateurs',
+      search: '',
       headers: [
         { text: 'E-mail', value: 'email', align: 'left' },
         { text: 'Contexte', value: 'context', align: 'left' },
-        { text: 'Confirmé', value: 'confirmed', align: 'left' }
+        { text: 'Confirmé', value: 'confirmed', align: 'left' },
+        { text: 'UUID', value: 'id', class: 'hidden' }
       ]
     }
   },
@@ -50,7 +56,17 @@ export default {
 }
 </script>
 
+<style lang="scss">
+  // thead.tr.th CSS is generated and does not work with scopes
+  th.hidden {
+    display: none;
+  }
+</style>
+
 <style lang="scss" scoped>
+  .hidden {
+    display: none;
+  }
   h2 {
     display: inline-block;
   }
