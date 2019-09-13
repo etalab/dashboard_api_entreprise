3d6b6ae5eccd5183986e1309ea96e8b5bd0233c7
Samuelfaure <samuel.faure.dev@gmail.com>
2019-09-13 16:57:27 +0200

    Added custom component-order rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,33 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-    "no-irregular-whitespace": "off"
+    "no-irregular-whitespace": "off",
+    "vue/order-in-components": [
+      "warn",
+      {
+        order: [
+          "el",
+          "name",
+          "parent",
+          "functional",
+          ["delimiters", "comments"],
+          ["directives", "filters"],
+          "extends",
+          "mixins",
+          "inheritAttrs",
+          "model",
+          ["props", "propsData"],
+          "data",
+          "computed",
+          "watch",
+          "LIFECYCLE_HOOKS",
+          "methods",
+          "components",
+          [("template", "render")],
+          "renderError"
+        ]
+      }
+    ]
   },
   parserOptions: {
     parser: "babel-eslint"
