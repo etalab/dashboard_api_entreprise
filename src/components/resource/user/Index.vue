822f3c7852d1430b3f8711dd64d4c3507f7b3f2f
Samuelfaure <samuel.faure.dev@gmail.com>
2019-09-17 18:06:45 +0200

    user array rebuild without vuetify, minus ordering and search

--- src/components/resource/user/Index.vue
+++ src/components/resource/user/Index.vue
@@ -1,22 +1,25 @@
 <template lang="pug">
   .main
     .panel
-      .panel__header
-        h2 Liste des utilisateurs
-        button.button.small.title-button(@click="userForm") Ajouter un utilisateur
+      //- .panel__header
+      //-   h2 Liste des utilisateurs
+      //-   button.button.small.title-button(@click="userForm") Ajouter un utilisateur
 
       .form__group
-        input(v-model="search", placeholder="recherche par UUID, email, contexte")
-
-      .form__group
-        v-data-table.elevation-1(:headers="headers", :items="userList", :search="search")
-          template(v-slot:items="props")
-            td
-              router-link(:to="{ name: 'admin-user-profile', params: { userId: props.item.id }}") {{ props.item.email }}
-            td {{ props.item.context }}
-            td {{ props.item.confirmed ? 'Oui' : 'Non' }}
-            td.hidden {{ props.item.id }}
-
+        .panel
+          h3 Liste des utilisateurs
+          button.button.small.title-button(@click="userForm") Ajouter un utilisateur
+          input.table__filter(v-model="search", placeholder="recherche par UUID, email, contexte")
+          table.table
+            thead
+              th.ascending E-mail
+              th.descending Contexte
+              th.ascending Confirmé
+            tr(v-for="user in userList")
+              td
+                router-link(:to="{ name: 'admin-user-profile', params: { userId: user.email }}") {{ user.email }}
+              td {{ user.context }}
+              td {{ user.confirmed }}
 </template>
 
 <script>
@@ -30,13 +33,7 @@ export default {
   data() {
     return {
       title: "Utilisateurs",
-      search: "",
-      headers: [
-        { text: "E-mail", value: "email", align: "left" },
-        { text: "Contexte", value: "context", align: "left" },
-        { text: "Confirmé", value: "confirmed", align: "left" },
-        { text: "UUID", value: "id", class: "hidden" }
-      ]
+      search: ""
     };
   },
 
@@ -56,24 +53,16 @@ export default {
 };
 </script>
 
-<style lang="scss">
-// thead.tr.th CSS is generated and does not work with scopes
-th.hidden {
-  display: none;
-}
-
-// fixes a display bug with our css
-.v-menu__content {
-  position: fixed !important;
-}
-</style>
-
 <style lang="scss" scoped>
-.hidden {
-  display: none;
-}
-h2 {
+.panel > h3,
+.panel > button {
   display: inline-block;
+  margin-left: 10px;
+  margin-right: 10px;
+}
+
+.panel > button {
+  float: right;
 }
 
 td,
